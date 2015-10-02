42c63b85e495ff4db2f6bae820060c0c68937b81
darkterra <darkterra01@gmail.com>
2015-10-02 08:49:27 +0000

    Revert "Correction orthographe sur le fichier README.md"
    This reverts commit fb4da17f3ff5eb681cc27e59c4abb11e3cf29c98.

--- server.js
+++ server.js
@@ -37,7 +37,6 @@ var socketio        = require('socket.io');
 var resumable       = require('./resumable-node.js')('tmp/');
 
 // Require des controllers
-var accueil         = require('./Controller/accueil');
 /*var compte          = require('./controllers/compte');
 var accueil         = require('./controllers/accueil');
 var oublie          = require('./controllers/oublie');
@@ -74,13 +73,11 @@ var io  = socketio.listen(http);
 
 // Template
 app.engine('html', require('ejs').renderFile);
-app.set('views', __dirname + '/View');
+app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
 
 // Routing
-app.use('/', accueil);
-app.use(express.static(path.join(__dirname, 'Public')));
-/*
+/*app.use(express.static(path.join(__dirname, 'public')));
 app.use('/compte', compte);
 app.use('/', accueil);
 app.use('/oublie', oublie);
@@ -172,7 +169,7 @@ http.listen(port, function () {
   console.log('La version du serveur Node.JS : '.data + process.version.warn);
   console.log('Le serveur Node.JS fonctionne sur la plateforme : '.data + process.platform.warn);
   //console.log('La plateforme fonctionne depuis : '.data + tools.convertTimeToHuman(os.uptime()).warn);
-  //console.log('Profileur du projet : https://nodetime.com/app/b3bf13f35870de/transactions\n'.info);
+  console.log('Profileur du projet : https://nodetime.com/app/b3bf13f35870de/transactions\n'.info);
 });
 
 
